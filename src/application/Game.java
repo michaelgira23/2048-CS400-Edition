8da46805498c3c3a6afccdf8f2e307a6a5442725
Faith I <fisaac2@wisc.edu>
2020-04-18 13:02:12 -0500

    Update Game.java

--- src/application/Game.java
+++ src/application/Game.java
@@ -1,4 +1,3 @@
-package application;
 
 /**
  * Keeps track of the internal game state
@@ -6,13 +5,46 @@ package application;
  * @author Michael Gira
  *
  */
+import java.util.Random;
+
 public class Game {
 
 	static final int HEIGHT = 4;
 	static final int WIDTH = 4;
+	
+	private int value = 1;
+	private int posX;
+	private int posY;
+	
+	private class GameSquare {
+		private GameSquare() {
+			value = 1;
+			Random ran = new Random();
+			posX = ran.nextInt(5);
+			posY = ran.nextInt(5);
+		}
+		public int increment() {
+			value *= 2;
+			return value;
+		}
+		public int getValue() {
+			return value;
+		}
+		public setPos(int x, int y) {
+			posX = x;
+			posY = y;
+		}	
+		public getX() {
+			return posX;
+		}
+		public getY() {
+			return posY;
+		}
+	}
 
 	private GameSquare[][] board = new GameSquare[HEIGHT][WIDTH];
 	
+
 	private void slideUpHelp() {
 		GameSquare tempGS = new GameSquare();
 		for(int r = 0; r < HEIGHT; r++) {
@@ -21,26 +53,51 @@ public class Game {
 				if(board[r][c] != null) {
 					tempGS = board[r][c];
 					int tempR = r;
-					//while it's not row 0 and there's no gamesquare above it keep moving the gamesquare up 
-					while(tempR > 0 && board[tempR-1][c] == null) {
-						board[tempR-1][c] = tempGS;
-						tempGS.setPos(c, tempR-1);
-						board[tempR][c] = null;
-						tempR--;
+					boolean repeat = true;
+					if(tempR == 0) {
+						repeat = false;
+						System.out.println("c");
 					}
-					//if theres a gamesquare above it (aka tempR is not 0 now)
-					if(tempR != 0) {
-						//if its value is equal to the gamesquare above it
-						if(board[tempR][c].getValue() == board[tempR-1][c].getValue()) {
-							board[tempR-1][c].increment();
+					else if(board[tempR - 1][c] != null) {
+						System.out.println("b");
+						repeat = false;
+					}
+					else {
+						//while it's not row 0 and there's no gamesquare above it keep moving the gamesquare up
+						while(repeat == true) {
+							board[tempR-1][c] = tempGS;
+							tempGS.setPos(c, tempR-1);
 							board[tempR][c] = null;
+							tempR--;
+							if(tempR == 0) {
+								repeat = false;
+								System.out.println("f");
+							}
+							else if(board[tempR - 1][c] != null) {
+								repeat = false;
+								System.out.println("g");
+							}
+						}	
+					}
+					//if theres a gamesquare above it (aka tempR is not 0)
+					if (tempR == 0) {
+						System.out.println("e");
+					}
+					else {
+						System.out.println("a");
+						if(board[tempR][c].getValue() == board[tempR-1][c].getValue()) {
+							System.out.println("h");
+							board[tempR][c] = null;
+							board[tempR-1][c].increment();
 						}
 					}
+					System.out.println(tempGS.getX() + "," + tempGS.getY());
 				}
 			}
 		}
 	}
 	
+		
 	private void slideDownHelp() {
 		GameSquare tempGS = new GameSquare();
 		for(int r = 3; r >= 0; r--) {
@@ -49,56 +106,72 @@ public class Game {
 				if(board[r][c] != null) {
 					tempGS = board[r][c];
 					int tempR = r;
-					//while it's not row 3 and there's no gamesquare below it keep moving the gamesquare down 
-					while(tempR < 3 && board[tempR+1][c] == null) {
-						board[tempR+1][c] = tempGS;
-						tempGS.setPos(c, tempR+1);
-						board[tempR][c] = null;
-						tempR++;
+					boolean repeat = true;
+					if(tempR == 3) {
+						repeat = false;
 					}
-					//if theres a gamesquare below it (aka tempR is not 3 now)
-					if(tempR != 3) {
-						//if its value is equal to the gamesquare above it
-						if(board[tempR][c].getValue() == board[tempR+1][c].getValue()) {
-							board[tempR+1][c].increment();
+					else if(board[tempR + 1][c] != null) {
+						repeat = false;
+					}
+					else {
+						//while it's not row 0 and there's no gamesquare above it keep moving the gamesquare up
+						while(repeat == true) {
+							board[tempR+1][c] = tempGS;
+							tempGS.setPos(c, tempR+1);
 							board[tempR][c] = null;
+							tempR++;
+							if(tempR == 3) {
+								repeat = false;
+							}
+							else if(board[tempR + 1][c] != null) {
+								repeat = false;
+							}
+						}	
+					}
+					//if theres a gamesquare above it (aka tempR is not 3)
+					if(tempR != 3) {
+						if(board[tempR][c].getValue() == board[tempR+1][c].getValue()) {
+							board[tempR][c] = null;
+							board[tempR+1][c].increment();
 						}
 					}
 				}
 			}
 		}
 	}
+	
+	
 		
 	
 	
 	private void slideLeftHelp() {
-		GameSquare tempGS = new GameSquare();
 		for(int r = 0; r < HEIGHT; r++) {
-			for(int c = 0; c < WIDTH; c++) {
-				//if the slot has a gamesquare
-				if(board[r][c] != null) {
-					tempGS = board[r][c];
-					int tempC = c;
-					//while it's not col 0 and there's no gamesquare to the left of it keep moving the gamesquare left 
-					while(tempC > 0 && board[r][tempC-1] == null) {
-						board[r][tempC] = tempGS;
-						tempGS.setPos(tempC-1, r);
-						board[r][tempC] = null;
-						tempC--;
-					}
-					//if theres a gamesquare to the left of it it (aka tempC is not 0 now)
-					if(tempC != 0) {
-						//if its value is equal to the gamesquare to the left of it
-						if(board[r][tempC].getValue() == board[r][tempC-1].getValue()) {
-							board[r][tempC-1].increment();
-							board[r][tempC] = null;
+			boolean combineOccurred = false;
+			int nextAvailable = 0;
+			if(board[r][0] != null) {
+				nextAvailable = 1;
+			}
+			for(int i = nextAvailable; i < board.length; i++) {
+				if(board[r][i] != null) {
+					board[r][nextAvailable] = board[r][i];
+					board[r][i] = null;
+					nextAvailable++;
+					
+					if(nextAvailable > 1 && combineOccurred == false) {
+						if(board[r][nextAvailable-1].getValue() == board[r][nextAvailable-2].getValue()) {
+							board[r][nextAvailable-2].increment();
+							board[r][nextAvailable-1] = null;
+							nextAvailable--;
 						}
 					}
 				}
+				
 			}
 		}
-		
+
 	}
+	
+	
 	
 	private void slideRightHelp() {
 		GameSquare tempGS = new GameSquare();
@@ -108,19 +181,33 @@ public class Game {
 				if(board[r][c] != null) {
 					tempGS = board[r][c];
 					int tempC = c;
-					//while it's not col 3 and there's no gamesquare to the right of it keep moving the gamesquare right 
-					while(tempC < 3 && board[r][tempC+1] == null) {
-						board[r][tempC] = tempGS;
-						tempGS.setPos(tempC+1, r);
-						board[r][tempC] = null;
-						tempC++;
+					boolean repeat = true;
+					if(tempC == 3) {
+						repeat = false;
 					}
-					//if theres a gamesquare to the right of it (aka tempC is not 3 now)
-					if(tempC != 3) {
-						//if its value is equal to the gamesquare to the right of it
-						if(board[r][tempC].getValue() == board[r][tempC+1].getValue()) {
-							board[r][tempC+1].increment();
+					else if(board[r][tempC+1] != null) {
+						repeat = false;
+					}
+					else {
+						//while it's not col 3 and there's no gamesquare above it keep moving the gamesquare up
+						while(repeat == true) {
+							board[r][tempC+1] = tempGS;
+							tempGS.setPos(tempC+1, r);
 							board[r][tempC] = null;
+							tempC++;
+							if(tempC == 3) {
+								repeat = false;
+							}
+							else if(board[r][tempC+1] != null) {
+								repeat = false;
+							}
+						}	
+					}
+					//if theres a gamesquare above it (aka tempR is not 3)
+					if(tempC < 3) {
+						if(board[r][tempC+1].getValue() == board[r][tempC].getValue()) {
+							board[r][tempC] = null;
+							board[r][tempC+1].increment();
 						}
 					}
 				}
@@ -185,6 +272,20 @@ public class Game {
 	 */
 	public GameSquare[][] getBoard() {
 		return board;
+	}
+	
+	public void print() {
+		for(int r = 0; r < HEIGHT; r++) {
+			for(int c = 0; c < WIDTH; c++) {
+				if(board[r][c] == null) {
+					System.out.print("/");
+				}
+				else {
+					System.out.print(board[r][c].getValue());
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
